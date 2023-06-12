a9e4f80246bec379946d26566c40922db9f07ba2
Parsa <parsa.khavarinejad@gmail.com>
2023-06-12 23:21:56 +0330

    full structure of the project. completed jupyter notebook version of the project

--- data_preprocessing/data_preprocessing.py
+++ data_preprocessing/data_preprocessing.py
@@ -4,73 +4,93 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.utils import resample
 
-ecg_test = pd.read_csv('../data/mitbih_test.csv', header=None)
-ecg_train = pd.read_csv('../data/mitbih_train.csv', header=None)
 
-ecg_train[187] = ecg_train[187].astype(int)
-counts = ecg_train[187].value_counts()
-counts = counts.reset_index()
-counts = counts.rename(columns={counts.columns[1]: 'class'})
+class Preprocessing:
 
-classes = {0: 'Non-ecotic beats (normal beat)',
-           1: 'Supraventricular ectopic beats',
-           2: 'Ventricular ectopic beats',
-           3: 'Fusion beat',
-           4: 'Unknown Beats'}
+    def __init__(self, train_path, test_path):
+        self.train_path = train_path
+        self.test_path = test_path
 
-class_counts = {}
-for class_number, class_name in classes.items():
-    class_counts[class_name] = counts['class'][counts.index == class_number].values[0]
+    def __read_data(self):
+        ecg_test = pd.read_csv(self.test_path, header=None)
+        ecg_train = pd.read_csv(self.test_path, header=None)
 
-for class_number, class_name in class_counts.items():
-    print('The length of {} is {}'.format(class_number, class_name))
+        return ecg_train, ecg_test
 
-plt.figure(figsize=(25, 15))
-plt.figure(figsize=(25, 15))
-counts['class'].plot(kind='pie', labels=['Non-ecotic beats (normal beat)',
-                                         'Unknown Beats',
-                                         'Ventricular ectopic beats',
-                                         'Supraventricular ectopic beats',
-                                         'Fusion Beats'], autopct='%1.1f%%')
-plt.axis('equal')
-plt.title('distribution of each class (types of diseases)')
-plt.show()
+    def vis(self):
+        ecg_train, ecg_test = self.__read_data()
+        ecg_train[187] = ecg_train[187].astype(int)
+        counts = ecg_train[187].value_counts()
+        counts = counts.reset_index()
+        counts = counts.rename(columns={counts.columns[1]: 'class'})
 
-for i in range(3):
-    plt.figure(figsize=(14, 10))
-    n = ecg_train.sample(1)
-    plt.plot(n.iloc[0, :186])
-    plt.title(classes[int(n[187])])
+        classes = {0: 'Non-ecotic beats (normal beat)',
+                   1: 'Supraventricular ectopic beats',
+                   2: 'Ventricular ectopic beats',
+                   3: 'Fusion beat',
+                   4: 'Unknown Beats'}
 
-    plt.show()
+        class_counts = {}
+        for class_number, class_name in classes.items():
+            class_counts[class_name] = counts['class'][counts.index == class_number].values[0]
 
-plt.figure(figsize=(17, 10))
-for i in range(5):
-    n = ecg_train.sample(1)
-    plt.plot(n.iloc[0, :186], label=classes[int(n[187])])
-    plt.title('Different types')
-    plt.legend(loc='best')
+        for class_number, class_name in class_counts.items():
+            print('The length of {} is {}'.format(class_number, class_name))
 
-train_upsample1 = ecg_train[ecg_train[187] == 1]
-train_upsample2 = ecg_train[ecg_train[187] == 2]
-train_upsample3 = ecg_train[ecg_train[187] == 3]
-train_upsample4 = ecg_train[ecg_train[187] == 4]
-train_upsample0 = (ecg_train[ecg_train[187] == 0]).sample(n=10000, random_state=1)
+        plt.figure(figsize=(25, 15))
+        plt.figure(figsize=(25, 15))
+        counts['class'].plot(kind='pie', labels=['Non-ecotic beats (normal beat)',
+                                                 'Unknown Beats',
+                                                 'Ventricular ectopic beats',
+                                                 'Supraventricular ectopic beats',
+                                                 'Fusion Beats'], autopct='%1.1f%%')
+        plt.axis('equal')
+        plt.title('distribution of each class (types of diseases)')
+        plt.show()
 
-train1_upsample = resample(train_upsample1, replace=True, n_samples=10000, random_state=43)
-train2_upsample = resample(train_upsample2, replace=True, n_samples=10000, random_state=44)
-train3_upsample = resample(train_upsample3, replace=True, n_samples=10000, random_state=54)
-train4_upsample = resample(train_upsample4, replace=True, n_samples=10000, random_state=65)
+        for i in range(3):
+            plt.figure(figsize=(14, 10))
+            n = ecg_train.sample(1)
+            plt.plot(n.iloc[0, :186])
+            plt.title(classes[int(n[187])])
 
-new_train = pd.concat([train_upsample0, train1_upsample, train2_upsample, train3_upsample, train4_upsample])
-new_train[187] = new_train[187].astype(int)
-counts = new_train[187].value_counts()
+            plt.show()
 
-plt.figure(figsize=(25, 15))
-counts.plot(kind='pie', labels=['Non-ecotic beats (normal beat)',
-                                'Unknown Beats',
-                                'Ventricular ectopic beats',
-                                'Supraventricular ectopic beats',
-                                'Fusion Beats'], autopct='%1.1f%%')
-plt.title("Upsampled data")
-plt.show()
+        plt.figure(figsize=(17, 10))
+        for i in range(5):
+            n = ecg_train.sample(1)
+            plt.plot(n.iloc[0, :186], label=classes[int(n[187])])
+            plt.title('Different types')
+            plt.legend(loc='best')
+            plt.show()
+
+        return ecg_train, ecg_test
+
+    def resampling_data(self):
+        ecg_train, ecg_test = self.vis()
+
+        train_upsample1 = ecg_train[ecg_train[187] == 1]
+        train_upsample2 = ecg_train[ecg_train[187] == 2]
+        train_upsample3 = ecg_train[ecg_train[187] == 3]
+        train_upsample4 = ecg_train[ecg_train[187] == 4]
+        train_upsample0 = (ecg_train[ecg_train[187] == 0]).sample(n=10000, random_state=1)
+
+        train1_upsample = resample(train_upsample1, replace=True, n_samples=10000, random_state=43)
+        train2_upsample = resample(train_upsample2, replace=True, n_samples=10000, random_state=44)
+        train3_upsample = resample(train_upsample3, replace=True, n_samples=10000, random_state=54)
+        train4_upsample = resample(train_upsample4, replace=True, n_samples=10000, random_state=65)
+
+        new_train = pd.concat([train_upsample0, train1_upsample, train2_upsample, train3_upsample, train4_upsample])
+        new_train[187] = new_train[187].astype(int)
+        counts = new_train[187].value_counts()
+
+        plt.figure(figsize=(25, 15))
+        counts.plot(kind='pie', labels=['Non-ecotic beats (normal beat)',
+                                        'Unknown Beats',
+                                        'Ventricular ectopic beats',
+                                        'Supraventricular ectopic beats',
+                                        'Fusion Beats'], autopct='%1.1f%%')
+        plt.title("Upsampled data")
+        plt.show()
+
+        return new_train
---
+++ script.py
@@ -0,0 +1,88 @@
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
+import warnings
+warnings.filterwarnings("ignore")
+
+from .data_preprocessing import Preprocessing
+from .model import ConvNet
+
+
+train_path = 'data/mitbih_train.csv.csv'
+test_path = 'data/mitbih_test.csv'
+preprocessing = Preprocessing(train_path, test_path)
+new_train = preprocessing.vis()
+
+X = new_train.iloc[:,:186]
+y = new_train.iloc[:,187]
+
+y = to_categorical(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = ConvNet()
+model.build(input_shape=(None, 186, 1))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+X_train = np.expand_dims(X_train, axis=-1)
+X_test = np.expand_dims(X_test, axis=-1)
+
+history = model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))
+
+# Evaluate the Model
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+print(history)
+fig1, ax_acc = plt.subplots()
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title('Model - Accuracy')
+plt.legend(['Training', 'Validation'], loc='lower right')
+plt.show()
+
+fig2, ax_loss = plt.subplots()
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Model- Loss')
+plt.legend(['Training', 'Validation'], loc='upper right')
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.show()
+target_names=['0','1','2','3','4']
+
+
+y_true = np.argmax(y_test, axis=1)
+prediction_proba = model.predict(X_test)
+prediction = np.argmax(prediction_proba, axis=1)
+cnf_matrix = confusion_matrix(y_true, prediction)
+
+plt.figure(figsize=(16, 9))
+x_axis_labels = ['Predicted normal', 'Predicted Unknown', 'Predicted Ventricular', 'Predicted Supraventricular', 'Predicted Fusion']
+y_axis_labels = ['normal', 'Unknown', 'Ventricular', 'Supraventricular', 'Fusion']
+sns.heatmap(cnf_matrix, annot=True, fmt="d", xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+
+
+ecg_test[187] = ecg_test[187].astype(int)
+counts = ecg_test[187].value_counts()
+
+X_ecg_test = ecg_test.iloc[:, :186]
+y_ecg_test = ecg_test.iloc[:, 187]
+X_ecg_test = X_ecg_test.values.reshape(-1, 186, 1)
+
+prediction_proba = model.predict(X_ecg_test)
+prediction = np.argmax(prediction_proba, axis=1)
+cnf_matrix = confusion_matrix(y_ecg_test, prediction)
+
+plt.figure(figsize=(16, 9))
+x_axis_labels = ['Predicted normal', 'Predicted Unknown', 'Predicted Ventricular', 'Predicted Supraventricular', 'Predicted Fusion']
+y_axis_labels = ['normal', 'Unknown', 'Ventricular', 'Supraventricular', 'Fusion']
+sns.heatmap(cnf_matrix, annot=True, fmt="d", xticklabels=x_axis_labels, yticklabels=y_axis_labels)
+
+target_names = ['Predicted normal', 'Predicted Unknown', 'Predicted Ventricular', 'Predicted Supraventricular', 'Predicted Fusion']
+print(classification_report(y_ecg_test, prediction, target_names=target_names))
+
+
+
